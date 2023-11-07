a77f43bd84f976ee717091e4eaa50f39bb6bd7d6
jordimall <jordi.mallafre@hotmail.com>
2023-11-07 19:19:35 +0100

    First commit

---
+++ src/main/java/com/example/demo/Ta25SpringRestErEjercicio04Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta25SpringRestErEjercicio04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta25SpringRestErEjercicio04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Ta25SpringRestErEjercicio04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta25SpringRestErEjercicio04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
